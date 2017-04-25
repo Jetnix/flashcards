fabc0ce46876113cba9da93cfc4bc16332331bb2
Chase Grijalva and Michael Koceja <chase@>
2017-04-25 15:11:34 -0700

    added buttons, definitions, and java toggles

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $(".click-me1").click(function() {
+    $("#jav-hidden").toggle();
+    $("#jav-show").toggle();
+  });
+
+  $(".click-me2").click(function() {
+    $("#oper-hidden").toggle();
+    $("#oper-show").toggle();
+  });
+
+  $(".click-me3").click(function() {
+    $("#variable-hidden").toggle();
+    $("#variable-show").toggle();
+  });
+
+  $(".click-me4").click(function() {
+    $("#vnc-hidden").toggle();
+    $("#vnc-show").toggle();
+  });
+
+  $(".click-me5").click(function() {
+    $("#funct-hidden").toggle();
+    $("#funct-show").toggle();
+  });
+
+  $(".click-me6").click(function() {
+    $("#methods-hidden").toggle();
+    $("#methods-show").toggle();
+  });
+
+  $(".click-me7").click(function() {
+    $("#argu-hidden").toggle();
+    $("#argu-show").toggle();
+  });
+
+  $(".click-me8").click(function() {
+    $("#para-hidden").toggle();
+    $("#para-show").toggle();
+  });
+
+  $(".click-me9").click(function() {
+    $("#retur-hidden").toggle();
+    $("#retur-show").toggle();
+  });
+
+  $(".click-me10").click(function() {
+    $("#chain-m-hidden").toggle();
+    $("#chain-m-show").toggle();
+  });
+
+  $(".click-me11").click(function() {
+    $("#string-hidden").toggle();
+    $("#string-show").toggle();
+  });
+
+  $(".click-me12").click(function() {
+    $("#boo-hidden").toggle();
+    $("#boo-show").toggle();
+  });
+
+  $(".click-me13").click(function() {
+    $("#undef-hidden").toggle();
+    $("#undef-show").toggle();
+  });
+
+  $(".click-me14").click(function() {
+    $("#nan-hidden").toggle();
+    $("#nan-show").toggle();
+  });
+
+  $(".click-me15").click(function() {
+    $("#escap-hidden").toggle();
+    $("#escap-show").toggle();
+  });
+
+  $(".click-me16").click(function() {
+    $("#alert-hidden").toggle();
+    $("#alert-show").toggle();
+  });
+
+  $(".click-me17").click(function() {
+    $("#comment-hidden").toggle();
+    $("#comment-show").toggle();
+  });
+
+  $(".click-me18").click(function() {
+    $("#jquery-hidden").toggle();
+    $("#jquery-show").toggle();
+  });
+
+  $(".click-me19").click(function() {
+    $("#attrib-hidden").toggle();
+    $("#attrib-show").toggle();
+  });
+});
